d87241d235cf3dc65b8aab8ff5c953f12f358315
Julien Abbadie <julien.abbadie@pytheascapital.com>
2023-11-29 10:16:36 +0100

    init mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import MainRoutes from './routes/main.routes'
+import { ThemeProvider } from '@emotion/react'
 import AuthenticatedRouteWrapper from './routes'
+import theme from './theme'
 
 export default function App() {
   return (
-    <Router>
-      <AuthenticatedRouteWrapper>
-        <MainRoutes />
-      </AuthenticatedRouteWrapper>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AuthenticatedRouteWrapper>
+          <MainRoutes />
+        </AuthenticatedRouteWrapper>
+      </Router>
+    </ThemeProvider>
   )
 }
--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,12 +1,31 @@
-import { NavLink, Outlet } from 'react-router-dom'
-
+import { Outlet } from 'react-router-dom'
+import { AppBar, Button, Toolbar, Typography } from '@mui/material'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
 export default function MainLayout() {
   return (
     <div>
-      <div className={'w-full text-white bg-black'}>
+      {/* <div className={'w-full text-white bg-black'}>
         <NavLink to={'/'}>Home</NavLink>
         <NavLink to={'/about'}>About</NavLink>
-      </div>
+      </div>*/}
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <Outlet />
     </div>
   )
